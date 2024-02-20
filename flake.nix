efa4c7929265bf7cdfc40eda5979463c38069496
zacharyarnaise <zacharyarnaise@users.noreply.github.com>
2024-02-20 19:13:00 +0000

    Auto format Nix files

--- flake.nix
+++ flake.nix
@@ -5,11 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
