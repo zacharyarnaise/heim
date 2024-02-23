9ce824d201fd7a3e3fd80834d32df4ca34e70fde
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-02-23 14:51:50 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,16 +1,75 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    systems = [
+      "x86_64-linux"
+    ];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system: import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    });
+  in {
+    inherit lib;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # Reusable NixOS and home-manager modules
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
+
+    # Custom packages and modifications, exported as overlays
+    overlays = import ./overlays { inherit inputs outputs; };
+
+    # Custom packages available through 'nix build', 'nix shell', etc.
+    packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
+    # Formatter available through 'nix fmt'
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#hostname'
+    nixosConfigurations = {
+        calcifer =  lib.nixosSystem {
+          modules = [ ./hosts/calcifer ];
+          specialArgs = { inherit inputs outputs; };
+        };
+
+        laptop-gb = lib.nixosSystem {
+          modules = [ ./hosts/laptop-gb ];
+          specialArgs = { inherit inputs outputs; };
+        };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#username@hostname'
+    homeConfigurations = {
+      "zach@calcifer" = lib.homeManagerConfiguration {
+        modules = [ ./home/zach/calcifer.nix ];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = { inherit inputs outputs; };
+      };
+
+      "zach@laptop-gb" = lib.homeManagerConfiguration {
+        modules = [ ./home/zach/laptop-gb.nix ];
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = { inherit inputs outputs; };
+      };
+    };
   };
 }
