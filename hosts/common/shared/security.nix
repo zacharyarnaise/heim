89ed2fe91b52d391de5ab7fdc1a0b450e7bd91eb
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-06-16 17:51:57 +0200

    wip

--- hosts/common/shared/security.nix
+++ hosts/common/shared/security.nix
@@ -1,28 +1,38 @@
 {
-  security = {
-    sudo = {
-      enable = true;
-      wheelNeedsPassword = true;
-    };
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = true;
   };
 
-  services = {
-    openssh = {
-      settings = {
-        KbdInteractiveAuthentication = false;
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
-        UseDns = false;
-        X11Forwarding = false;
+  programs.ssh = {
+    enableAskPassword = false;
+    hostKeyAlgorithms = [
+      "ssh-ed25519"
+      "ecdsa-sha2-nistp256"
+      "ecdsa-sha2-nistp384"
+      "ecdsa-sha2-nistp521"
+      "rsa-sha2-512"
+      "rsa-sha2-256"
+      "ssh-rsa"
+    ];
+  };
 
-        Ciphers = [
-          "chacha20-poly1305@openssh.com"
-        ];
-        KexAlgorithms = [
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-        ];
-      };
+  services.openssh = {
+    allowSFTP = false;
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      UseDns = false;
+      X11Forwarding = false;
+
+      Ciphers = [
+        "chacha20-poly1305@openssh.com"
+      ];
+      KexAlgorithms = [
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+      ];
     };
   };
 }
