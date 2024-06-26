c5f3121a0598f2fa58bdfaaad6c5f2cb1ce483e4
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-06-26 21:22:55 +0200

    wip

--- hosts/laptop-gb/disko.nix
+++ hosts/laptop-gb/disko.nix
@@ -1,54 +1,56 @@
-_: {
+{
   disko.devices = {
-    disk.main = {
-      device = "/dev/nvme0n1";
-      type = "disk";
+    disk = {
+      nvme0n1 = {
+        device = "/dev/nvme0n1";
+        type = "disk";
 
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            type = "EF00";
-            size = "500M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = ["defaults" "noauto" "noexec"];
-            };
-          };
-
-          luks = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "crypted";
-              askPassword = true;
-              settings = {
-                allowDiscards = true;
-                bypassWorkqueues = true;
-              };
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
               content = {
-                type = "btrfs";
-                extraArgs = ["-f"];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                    mountOptions = ["compress=lzo" "noatime"];
-                  };
-                  "/persist" = {
-                    mountOptions = ["compress=lzo" "noatime"];
-                  };
-                  "/nix" = {
-                    mountOptions = [
-                      "compress=lzo"
-                      "noatime"
-                      "lazytime"
-                      "noacl"
-                    ];
-                  };
-                  "/var/log" = {
-                    mountOptions = ["compress=lzo" "noatime" "lazytime"];
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["defaults" "noauto" "noexec"];
+              };
+            };
+
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                askPassword = true;
+                settings = {
+                  allowDiscards = true;
+                  bypassWorkqueues = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=lzo" "noatime"];
+                    };
+                    "/persist" = {
+                      mountOptions = ["compress=lzo" "noatime"];
+                    };
+                    "/nix" = {
+                      mountOptions = [
+                        "compress=lzo"
+                        "noatime"
+                        "lazytime"
+                        "noacl"
+                      ];
+                    };
+                    "/var/log" = {
+                      mountOptions = ["compress=lzo" "noatime" "lazytime"];
+                    };
                   };
                 };
               };
