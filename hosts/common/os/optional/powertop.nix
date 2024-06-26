cf464c0deb615fdf65dcb0719b60b6ba2dcbab74
zacharyarnaise <zacharyarnaise@users.noreply.github.com>
2024-06-26 23:51:57 +0000

    Auto format Nix files

--- hosts/common/os/optional/powertop.nix
+++ hosts/common/os/optional/powertop.nix
@@ -5,7 +5,7 @@
 }: {
   powerManagement.powertop.enable = true;
   services.tlp.enable = true;
-  systemd.services.tlp = lib.mkIf (config.services.tlp.enable) {
+  systemd.services.tlp = lib.mkIf config.services.tlp.enable {
     after = ["powertop.service"];
   };
 }
