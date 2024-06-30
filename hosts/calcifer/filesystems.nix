df6f2fa2f9aa839c56a04535248c96fdb889318b
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-06-30 22:48:06 +0200

    calcifer: fix `/boot` mount options

--- hosts/calcifer/filesystems.nix
+++ hosts/calcifer/filesystems.nix
@@ -11,7 +11,7 @@
     "/boot" = {
       device = "/dev/disk/by-label/ESP";
       fsType = "vfat";
-      options = ["defaults" "noauto" "noexec"];
+      options = ["defaults" "noexec" "noauto" "x-systemd.automount"];
     };
 
     "/" = {
