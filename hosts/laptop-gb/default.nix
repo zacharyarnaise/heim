1c67c867eea22594fc156b1016097869dc0aba1e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-06-30 17:04:02 +0200

    wip

--- hosts/laptop-gb/default.nix
+++ hosts/laptop-gb/default.nix
@@ -6,13 +6,13 @@
     ../common/users/zach
 
     ../common/os/optional/bluetooth.nix
-    ../common/os/optional/boot-quiet.nix
+    # ../common/os/optional/boot-quiet.nix
     ../common/os/optional/powertop.nix
     ../common/os/optional/wireless-wpa_supplicant.nix
   ];
 
   # FIXME: remove when debugging is done
-  users.users.root.initialPassword = "nixos";
+  users.users.root.password = "nixos";
 
   networking.hostName = "laptop-gb";
 
