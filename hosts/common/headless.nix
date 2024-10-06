4ffc1182e2d9c5c78bf730566517abd06aa5552b
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-10-06 18:37:12 +0200

    hosts: add `howl` (#37)

---
+++ hosts/common/headless.nix
@@ -0,0 +1,28 @@
+# Configuration applicable to my headless devices
+{lib, ...}: {
+  imports = [
+    ./all
+
+    ./optional/resolved.nix
+    ./optional/impermanence.nix
+  ];
+
+  boot.kernelParams = ["nowatchdog"];
+
+  networking = {
+    nameservers = [
+      "1.1.1.1#cloudflare-dns.com"
+      "1.0.0.1#cloudflare-dns.com"
+      "2606:4700:4700::1111#cloudflare-dns.com"
+      "2606:4700:4700::1001#cloudflare-dns.com"
+    ];
+    useDHCP = lib.mkDefault true;
+  };
+
+  nix = {
+    daemonCPUSchedPolicy = "batch";
+    daemonIOSchedClass = "best-effort";
+  };
+
+  hardware.graphics.enable = lib.mkForce false;
+}
