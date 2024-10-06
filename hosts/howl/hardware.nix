443c3eb3479689d878a58b3ed45784835ec4469b
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-10-06 18:31:31 +0200

    wip

---
+++ hosts/howl/hardware.nix
@@ -0,0 +1,22 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  imports = with inputs.nixos-hardware.nixosModules; [
+    raspberry-pi-5
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [
+      "nvme"
+      "usbhid"
+    ];
+  };
+
+  nix.settings.max-jobs = 4;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  swapDevices = lib.mkForce [];
+}
