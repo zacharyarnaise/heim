4d3b5b5e26b0a61704466df167f3eeb02b5ae1fc
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-10-20 19:48:56 +0200

    flake.nix: remove redundant imports

--- flake.nix
+++ flake.nix
@@ -128,37 +128,25 @@
     # Available through 'home-manager --flake .#username@hostname'
     homeConfigurations = {
       "zach@calcifer" = nixpkgs.lib.homeManagerConfiguration {
-        modules = [
-          ./home/zach/all
-          ./home/zach/calcifer.nix
-        ];
+        modules = [./home/zach/calcifer.nix];
         pkgs = nixpkgsFor.x86_64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
       };
 
       "zach@howl" = nixpkgs.lib.homeManagerConfiguration {
-        modules = [
-          ./home/zach/all
-          ./home/zach/howl.nix
-        ];
+        modules = [./home/zach/howl.nix];
         pkgs = nixpkgsFor.aarch64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
       };
 
       "zach@laptop-gb" = nixpkgs.lib.homeManagerConfiguration {
-        modules = [
-          ./home/zach/all
-          ./home/zach/laptop-gb.nix
-        ];
+        modules = [./home/zach/laptop-gb.nix];
         pkgs = nixpkgsFor.x86_64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
       };
 
       "zach@noface" = nixpkgs.lib.homeManagerConfiguration {
-        modules = [
-          ./home/zach/all
-          ./home/zach/noface.nix
-        ];
+        modules = [./home/zach/noface.nix];
         pkgs = nixpkgsFor.x86_64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
       };
