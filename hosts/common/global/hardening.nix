4307849fea3b15ba4f4688fe5620ff4252cf39d3
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-11-06 00:25:37 +0100

    wip

--- hosts/common/global/hardening.nix
+++ hosts/common/global/hardening.nix
@@ -52,4 +52,35 @@
       # See securetty(5) and login(1).
     '';
   };
+
+  boot.kernelParams = ["debugfs=off"];
+  boot.blacklistedKernelModules = [
+    # Obscure network protocols
+    "ax25"
+    "netrom"
+    "rose"
+
+    # Old or rare or insufficiently audited filesystems
+    "adfs"
+    "affs"
+    "bfs"
+    "befs"
+    "cramfs"
+    "efs"
+    "erofs"
+    "exofs"
+    "freevxfs"
+    "f2fs"
+    "hfs"
+    "hpfs"
+    "jfs"
+    "minix"
+    "nilfs2"
+    "ntfs"
+    "omfs"
+    "qnx4"
+    "qnx6"
+    "sysv"
+    "ufs"
+  ];
 }
