761316c07a028f65d9f7ff5996fcda8728d647b5
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-11-09 01:45:56 +0100

    wip

--- home/zach/all/default.nix
+++ home/zach/all/default.nix
@@ -17,6 +17,9 @@
     username = lib.mkDefault "zach";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
     sessionPath = ["$HOME/.local/bin"];
+    sessionVariables = {
+      FLAKE = "$HOME/heim";
+    };
 
     persistence = {
       "/persist/${config.home.homeDirectory}" = {
@@ -25,7 +28,6 @@
           "Downloads"
           "Pictures"
           "Videos"
-          ".local/bin"
           ".local/share/nix" # trusted settings and repl history
         ];
         allowOther = true;
