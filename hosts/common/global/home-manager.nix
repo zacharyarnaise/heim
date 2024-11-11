97de93c4ac29e57b4d7790f64c3ed3c23cef5420
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2024-11-11 17:07:08 +0100

    hm: add tmux

--- hosts/common/global/home-manager.nix
+++ hosts/common/global/home-manager.nix
@@ -1,5 +1,10 @@
-{inputs, ...}: {
+{
+  inputs,
+  outputs,
+  ...
+}: {
   imports = [inputs.home-manager.nixosModules.home-manager];
 
   home-manager.useGlobalPkgs = true;
+  home-manager.extraSpecialArgs = {inherit inputs outputs;};
 }
