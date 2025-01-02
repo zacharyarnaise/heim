1ed9fd4ccbd890438b342794570e64d9f3e17fa3
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-02 23:18:28 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -11,15 +11,15 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
 
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
