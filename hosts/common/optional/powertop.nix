1b628395238c13b3f891ffded2cf52b4446abf84
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-13 12:02:57 +0100

    wip

--- hosts/common/optional/powertop.nix
+++ hosts/common/optional/powertop.nix
@@ -3,6 +3,13 @@
   lib,
   ...
 }: {
+  environment.persistence."/persist/var/cache/powertop" = {
+    files = [
+      "/var/cache/powertop/saved_parameters.powertop"
+      "/var/cache/powertop/saved_results.powertop"
+    ];
+  };
+
   powerManagement.powertop.enable = true;
   services.tlp.enable = true;
   systemd.services.tlp = lib.mkIf config.services.tlp.enable {
