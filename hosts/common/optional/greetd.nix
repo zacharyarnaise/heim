24a2cdb7248f6d09b275159032ef18f181448473
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-20 23:16:37 +0100

    wip

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -11,7 +11,7 @@
           --asterisks \
           --no-xsession-wrapper \
           --sessions ${pkgs.hyprland}/share/wayland-sessions \
-          --theme greet=lightgreen;border=green
+          --theme 'greet=lightgreen;border=green'
         '';
         user = "greeter";
       };
