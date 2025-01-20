39f703f426b8f2b22914d31b20a54b8f8e2452ed
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-20 23:30:05 +0100

    wip

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -20,12 +20,12 @@
       allowed-users = lib.mkDefault [""]; # Trusted users are always allowed to connect
       trusted-users = lib.mkForce ["@wheel"];
       substituters = [
-        "https://nix-config.cachix.org"
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
-        "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
 
       connect-timeout = 5;
