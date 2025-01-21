7715e0225047973abcfb08b403ec68f6fce781ca
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-21 19:44:48 +0100

    wip

--- home/zach/features/de/hyprland/hyprlock.nix
+++ home/zach/features/de/hyprland/hyprlock.nix
@@ -8,6 +8,23 @@
         hide_cursor = true;
         grace = 10;
       };
+
+      background = {
+        blur_passes = 4;
+        blur_size = 8;
+        vibrancy_darkness = 0.04;
+      };
+
+      input-field = {
+        size = "400, 80";
+        position = "0, 0";
+        dots_center = true;
+        fade_on_empty = true;
+        outline_thickness = 3;
+        placeholder_text = "sup?";
+        shadow_passes = 2;
+        shadow_size = 4;
+      };
     };
   };
 }
