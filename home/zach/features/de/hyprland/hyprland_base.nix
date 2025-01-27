14c3e476fa4ff770e98035017189cb0d049dd7a3
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-28 00:38:15 +0100

    wip

--- home/zach/features/de/hyprland/hyprland_base.nix
+++ home/zach/features/de/hyprland/hyprland_base.nix
@@ -1,16 +1,19 @@
-{pkgs, ...}: let
-  hyprland = pkgs.hyprland.override {enableXWayland = false;};
-in {
+{
+  config,
+  pkgs,
+  ...
+}: {
   xdg.portal = let
+    hyprland = config.wayland.windowManager.hyprland.package;
     xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
   in {
-    configPackages = [hyprland];
     extraPortals = [xdph];
+    configPackages = [hyprland];
   };
 
   wayland.windowManager.hyprland = {
     enable = true;
-    package = hyprland;
+    package = pkgs.hyprland.override {enableXWayland = false;};
 
     systemd = {
       enable = true;
