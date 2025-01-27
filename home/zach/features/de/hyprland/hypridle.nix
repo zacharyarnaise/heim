06b1a4188ff371e6f16535c6ed15d85ed9ea7408
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-28 00:15:48 +0100

    wip

--- home/zach/features/de/hyprland/hypridle.nix
+++ home/zach/features/de/hyprland/hypridle.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: let
-  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
+  hyprctl = "hyprctl";
   hyprlock = "${config.programs.hyprlock.package}/bin/hyprlock";
   loginctl = "${pkgs.systemd}/bin/loginctl";
 in {
