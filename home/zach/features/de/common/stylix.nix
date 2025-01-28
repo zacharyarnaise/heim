a848c3d3d00a0ac16546348b1be40377931a51c0
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-28 17:01:10 +0100

    wip

--- home/zach/features/de/common/stylix.nix
+++ home/zach/features/de/common/stylix.nix
@@ -9,7 +9,7 @@
     enable = true;
 
     base16Scheme = "${inputs.tinted-theming}/base16/tokyo-night-moon.yaml";
-    image = ../../../assets/13.jpg;
+    image = ../../../assets/13.jpeg;
     polarity = "dark";
     cursor = {
       package = pkgs.catppuccin-cursors.mochaDark;
