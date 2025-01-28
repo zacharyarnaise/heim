c60a78a74ceea9577637c41b41dbfb619b2f0da1
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-28 11:19:41 +0100

    avoid with pkgs

--- home/zach/features/cli/common/bat.nix
+++ home/zach/features/cli/common/bat.nix
@@ -11,7 +11,7 @@
       style = "numbers,changes,header";
     };
     extraPackages = builtins.attrValues {
-      inherit (pkgs) batdiff batman batgrep;
+      inherit (pkgs.bat-extras) batdiff batman batgrep;
     };
   };
 
