28a073693ef6798d530690437c9361b3d108366e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-28 17:40:05 +0100

    wip

--- home/zach/features/de/hyprland/hyprland_settings.nix
+++ home/zach/features/de/hyprland/hyprland_settings.nix
@@ -141,6 +141,41 @@ in {
       new_window_takes_over_fullscreen = 2;
     };
 
+    monitor = let
+      waybarSpace = let
+        inherit (config.wayland.windowManager.hyprland.settings.general) gaps_in gaps_out;
+        inherit (config.programs.waybar.settings.mainBar) position height width;
+        gap = gaps_out - gaps_in;
+      in {
+        top =
+          if (position == "top")
+          then height + gap
+          else 0;
+        bottom =
+          if (position == "bottom")
+          then height + gap
+          else 0;
+        left =
+          if (position == "left")
+          then width + gap
+          else 0;
+        right =
+          if (position == "right")
+          then width + gap
+          else 0;
+      };
+    in
+      [
+        ",addreserved,${toString waybarSpace.top},${toString waybarSpace.bottom},${toString waybarSpace.left},${toString waybarSpace.right}"
+      ]
+      ++ (map (
+        m: "${m.name},${
+          if m.enabled
+          then "${toString m.width}x${toString m.height}@${toString m.refreshRate},${m.position},1"
+          else "disable"
+        }"
+      ) (config.monitors));
+
     #ecosystem = {
     #  no_update_news = true;
     #  no_donation_nag = true;
--- home/zach/laptop-gb.nix
+++ home/zach/laptop-gb.nix
@@ -5,4 +5,14 @@
     ./features/de/common
     ./features/de/hyprland
   ];
+
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1080;
+      workspace = "1";
+      primary = true;
+    }
+  ];
 }
