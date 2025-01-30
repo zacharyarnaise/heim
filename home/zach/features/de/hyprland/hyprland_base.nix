b61b66371c04e2d12c1bc3d39070d6a693221c6c
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-01-30 13:17:24 +0100

    hm/hyprland: re-enable xwayland

--- home/zach/features/de/hyprland/hyprland_base.nix
+++ home/zach/features/de/hyprland/hyprland_base.nix
@@ -6,9 +6,6 @@
   wayland.windowManager.hyprland = {
     enable = true;
 
-    xwayland.enable = false;
-    settings.xwayland.enabled = false;
-
     systemd = {
       enable = true;
       variables = ["--all"];
