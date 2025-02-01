22204fbb521779772ecdc1600f4043a9f806c5e9
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-02-01 18:59:48 +0100

    hosts: revert nix allow-import-from-derivation

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -13,7 +13,7 @@
     settings = {
       accept-flake-config = true;
       auto-optimise-store = lib.mkDefault false;
-      allow-import-from-derivation = lib.mkDefault false;
+      allow-import-from-derivation = lib.mkDefault true;
       allowed-users = lib.mkDefault [""]; # Trusted users are always allowed to connect
       trusted-users = lib.mkForce ["@wheel"];
       extra-substituters = lib.mkAfter [
