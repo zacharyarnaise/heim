bb5ba82bad40c1790fba9ef4481e5d938a7415a8
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-02-16 16:31:40 +0100

    hm: tweak gtk settings

--- home/zach/features/de/common/gtk.nix
+++ home/zach/features/de/common/gtk.nix
@@ -1,7 +1,14 @@
 {pkgs, ...}: {
   gtk = let
     commonExtraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-decoration-layout = "appmenu";
+      gtk-enable-event-sounds = 0;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-error-bell = 0;
       gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
     };
   in {
     enable = true;
