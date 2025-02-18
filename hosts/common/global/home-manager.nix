d144cc52d017d85f486d2556663d51f4fda8e9dc
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-02-18 16:08:50 +0100

    chore: pass hostSpec to home-manager

--- hosts/common/global/home-manager.nix
+++ hosts/common/global/home-manager.nix
@@ -1,10 +1,14 @@
 {
   inputs,
   outputs,
+  config,
   ...
 }: {
   imports = [inputs.home-manager.nixosModules.home-manager];
 
   home-manager.useGlobalPkgs = true;
-  home-manager.extraSpecialArgs = {inherit inputs outputs;};
+  home-manager.extraSpecialArgs = {
+    inherit inputs outputs;
+    hostSpec = config.hostSpec;
+  };
 }
