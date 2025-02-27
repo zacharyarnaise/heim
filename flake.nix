fac1ded3f85eefdd3d4822c4f632b0e6b9865430
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-02-27 18:47:41 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -78,7 +78,7 @@
 
     mkHome = username: hostname: system: {
       name = "${username}@${hostname}";
-      value = lib.homeManagerConfiguration {
+      value = inputs.home-manager.lib.homeManagerConfiguration {
         extraSpecialArgs = {
           inherit lib inputs;
           inherit (import ./hosts/${hostname}/spec.nix) hostSpec;
