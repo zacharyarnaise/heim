1448fbd96b8da56aba01566f2147e99871199d47
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-02-27 12:30:12 +0100

    wip

--- hosts/common/global/home-manager.nix
+++ hosts/common/global/home-manager.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   outputs,
+  config,
   ...
 }: {
   imports = [inputs.home-manager.nixosModules.home-manager];
@@ -8,5 +9,6 @@
   home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = {
     inherit inputs outputs;
+    inherit (config) hostSpec;
   };
 }
