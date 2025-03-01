b67060fec8614b1a7e5bf5e57eee196d9c7d3369
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-01 18:14:41 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
     # -- NixOS configurations --------------------------------------------------
     nixosConfigurations = lib.listToAttrs [
       (mkNixos "calcifer" "x86_64-linux")
-      (mkNixos "howl" "x86_64-linux")
+      #(mkNixos "howl" "x86_64-linux")
       (mkNixos "laptop-gb" "x86_64-linux")
       (mkNixos "noface" "x86_64-linux")
     ];
@@ -104,7 +104,7 @@
     # -- home-manager configurations -------------------------------------------
     homeConfigurations = lib.listToAttrs [
       (mkHome "zach" "calcifer" "x86_64-linux")
-      (mkHome "zach" "howl" "x86_64-linux")
+      #(mkHome "zach" "howl" "x86_64-linux")
       (mkHome "zach" "laptop-gb" "x86_64-linux")
       (mkHome "zach" "noface" "x86_64-linux")
     ];
