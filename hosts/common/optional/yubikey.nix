76bdfdfc5a9f788df2aa701e735d1c1dd52eb3a7
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-11 15:46:38 +0100

    wip

--- hosts/common/optional/yubikey.nix
+++ hosts/common/optional/yubikey.nix
@@ -11,7 +11,7 @@
   hardware.gpgSmartcards.enable = true;
   services = {
     pcscd.enable = true;
-    services.udev.packages = [pkgs.yubikey-personalization];
+    udev.packages = [pkgs.yubikey-personalization];
     yubikey-agent.enable = true;
   };
 
