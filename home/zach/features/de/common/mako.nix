9e4658449c0f15a2b7bf4c3c53f27f7db32f1278
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-13 10:46:32 +0100

    hm: instruct home icon path to mako

--- home/zach/features/de/common/mako.nix
+++ home/zach/features/de/common/mako.nix
@@ -1,15 +1,17 @@
 {config, ...}: let
-  iconTheme =
+  iconThemeName =
     if (config.stylix.polarity == "dark")
     then config.stylix.iconTheme.dark
     else config.stylix.iconTheme.light;
+  iconThemePath = "${config.stylix.iconTheme.package}/share/icons/${iconTheme}";
+  iconHomePath = "${config.home.homeDirectory}/.nix-profile/share/icons/hicolor";
 in {
   services.mako = {
     enable = true;
 
     actions = true;
     icons = true;
-    iconPath = "${config.stylix.iconTheme.package}/share/icons/${iconTheme}";
+    iconPath = "${iconThemePath}:${iconHomePath}";
     format = "<span font_scale='subscript' rise='6pt' text_transform='capitalize'>%a</span>\\n<b>%s</b>\\n%b";
     defaultTimeout = 4000;
     maxVisible = 5;
