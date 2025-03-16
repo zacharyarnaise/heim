649cf26233fa5bf44591bee7716fd49ce72f63f5
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-16 22:43:18 +0100

    flake: fix shell declaration

--- shell.nix
+++ shell.nix
@@ -1,19 +1,18 @@
-{pkgs ? import <nixpkgs> {}, ...}: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-    nativeBuildInputs = builtins.attrValues {
-      inherit
-        (pkgs)
-        nix
-        home-manager
-        deadnix
-        stylix
-        git
-        age
-        sops
-        ssh-to-age
-        sbctl
-        ;
-    };
+{pkgs ? import <nixpkgs> {}, ...}:
+pkgs.mkShell {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  nativeBuildInputs = builtins.attrValues {
+    inherit
+      (pkgs)
+      nix
+      home-manager
+      deadnix
+      stylix
+      git
+      age
+      sops
+      ssh-to-age
+      sbctl
+      ;
   };
 }
