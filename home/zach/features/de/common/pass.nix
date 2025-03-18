8dad3fbf56299a87b1b239da267c8079a1aa64a5
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-18 19:37:12 +0100

    wip

--- home/zach/features/de/common/pass.nix
+++ home/zach/features/de/common/pass.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  storePath = "/persist${config.home.homeDirectory}/.local/share/gopass/stores/root";
+in {
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
@@ -6,4 +12,20 @@
       gopass-jsonapi
       ;
   };
+
+  home.file = {
+    ".config/gopass/config".text = ''
+      [mounts]
+          path = ${storePath}
+    '';
+  };
+
+  home.activation.gopassStore = ''
+    if [ ! -d ${storePath} ] ; then
+      mkdir -m 0750 -p ${storePath}
+      git clone --depth=1 git@github.com:zacharyarnaise/pass.git ${storePath}
+    fi
+
+    echo "Y" | gopass-jsonapi configure --browser firefox --global=false --path=${config.home.homeDirectory}/.config/gopass
+  '';
 }
