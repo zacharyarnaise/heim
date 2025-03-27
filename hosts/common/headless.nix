f2a1a4ec0d3c31f68a60a1d256f73c33a27a0a6a
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-03-27 20:04:47 +0100

    wip

--- hosts/common/headless.nix
+++ hosts/common/headless.nix
@@ -11,14 +11,14 @@
     daemonIOSchedClass = "best-effort";
   };
 
-  # boot.kernelParams = ["nomodeset" "panic=1" "boot.panic_on_fail=1"];
-  # systemd = {
-  #   enableEmergencyMode = false;
-  #   services = {
-  #     "serial-getty@ttyS0".enable = lib.mkDefault false;
-  #     "serial-getty@hvc0".enable = false;
-  #     "getty@tty1".enable = false;
-  #     "autovt@".enable = false;
-  #   };
-  # };
+  boot.kernelParams = ["nomodeset" "panic=1" "boot.panic_on_fail=1"];
+  systemd = {
+    enableEmergencyMode = false;
+    services = {
+      "serial-getty@ttyS0".enable = lib.mkDefault false;
+      "serial-getty@hvc0".enable = false;
+      "getty@tty1".enable = false;
+      "autovt@".enable = false;
+    };
+  };
 }
