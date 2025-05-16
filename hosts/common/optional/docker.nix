51d3aadca15e7fa606557bb18c5e3cea20dda1be
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 16:53:19 +0200

    hosts: revert docker changes

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,12 +1,8 @@
-{config, ...}: {
+{
   virtualisation.docker = {
     enable = true;
 
     autoPrune.enable = true;
     autoPrune.dates = "monthly";
-    logDriver =
-      if config.hostSpec.isWork
-      then "none"
-      else "journald";
   };
 }
