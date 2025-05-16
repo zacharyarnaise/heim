b4017489770b1b80afc4b1aa01975e0763f5faaa
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 17:27:18 +0200

    hosts: docker logdriver to file

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,8 +1,13 @@
-{
+{lib, ...}: {
   virtualisation.docker = {
     enable = true;
 
     autoPrune.enable = true;
     autoPrune.dates = "monthly";
+    logDriver = "local";
+    extraOptions = lib.concatStringsSep " " [
+      "log-opt max-size=10m"
+      "log-opt max-file=1"
+    ];
   };
 }
