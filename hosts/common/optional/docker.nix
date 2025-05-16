a00f66ad4f9e4505bb825c973a6cfc4e3b4b2535
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 17:29:12 +0200

    hosts: docker set loglevel

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -6,6 +6,7 @@
     autoPrune.dates = "monthly";
     logDriver = "local";
     extraOptions = lib.concatStringsSep " " [
+      "log-level=warn"
       "log-opt max-size=10m"
       "log-opt max-file=1"
     ];
