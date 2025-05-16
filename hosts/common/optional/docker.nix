12505cf785ee331dabf7a15a17a2bde53d65e8c9
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 17:29:56 +0200

    hosts: docker set loglevel

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -6,9 +6,9 @@
     autoPrune.dates = "monthly";
     logDriver = "local";
     extraOptions = lib.concatStringsSep " " [
-      "log-level=warn"
-      "log-opt max-size=10m"
-      "log-opt max-file=1"
+      "--log-level=warn"
+      "--log-opt max-size=10m"
+      "--log-opt max-file=1"
     ];
   };
 }
