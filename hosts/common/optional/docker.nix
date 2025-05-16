364157b3be2db143c2f2466a709ec5f8937f3a5b
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 15:34:52 +0200

    hosts: stop docker spam in journal

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,8 +1,12 @@
-{
+{config, ...}: {
   virtualisation.docker = {
     enable = true;
 
     autoPrune.enable = true;
     autoPrune.dates = "monthly";
+    logDriver =
+      if config.hostSpec.isWork
+      then "none"
+      else "journald";
   };
 }
