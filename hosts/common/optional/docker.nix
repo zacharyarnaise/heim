60bfecc69cf4b48986151a973434936e9837295e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-16 16:29:07 +0200

    hosts: other workaround for docker log spam

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -8,13 +8,9 @@
 
     autoPrune.enable = true;
     autoPrune.dates = "monthly";
-    logDriver =
-      if config.hostSpec.isWork
-      then "none"
-      else "journald";
   };
 
   virtualisation.docker.daemon.settings = lib.mkIf config.hostSpec.isWork {
-    "--log-level" = "error";
+    "log-level" = "error";
   };
 }
