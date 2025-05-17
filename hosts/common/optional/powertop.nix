f8e5869b03ccaf43ce7dd76424eab5c933874a19
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-17 19:19:08 +0200

    hosts: set config for TLP

--- hosts/common/optional/powertop.nix
+++ hosts/common/optional/powertop.nix
@@ -11,8 +11,17 @@
   };
 
   powerManagement.powertop.enable = true;
-  services.tlp.enable = true;
   systemd.services.tlp = lib.mkIf config.services.tlp.enable {
     after = ["powertop.service"];
+  };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
   };
 }
