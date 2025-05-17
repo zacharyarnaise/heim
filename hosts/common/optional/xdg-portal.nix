901eb0bb539af1ac6d3f7f158555c5b73c805ae6
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-17 16:04:46 +0200

    hosts: remove unneeded xdg-portal config

--- hosts/common/optional/xdg-portal.nix
+++
@@ -1,7 +0,0 @@
-{
-  # see: https://github.com/nix-community/home-manager/pull/5184
-  environment.pathsToLink = [
-    "/share/xdg-desktop-portal"
-    "/share/applications"
-  ];
-}
--- hosts/common/pc.nix
+++ hosts/common/pc.nix
@@ -6,7 +6,6 @@
     ./optional/docker.nix
     ./optional/greetd.nix
     ./optional/hyprland.nix
-    ./optional/xdg-portal.nix
     ./optional/pipewire.nix
     ./optional/yubikey.nix
     ./optional/secureboot.nix
