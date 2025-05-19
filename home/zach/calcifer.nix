7f15c8a5497c16c2ba6e8dbb8354204b760590c3
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-05-20 00:07:43 +0200

    hm/calcifer: declare monitors and input devs

--- home/zach/calcifer.nix
+++ home/zach/calcifer.nix
@@ -8,4 +8,32 @@
 
     ./features/dev
   ];
+
+  monitors = [
+    {
+      name = "DP-3";
+      width = 3840;
+      height = 2160;
+      refreshRate = 144;
+      workspace = "3";
+      primary = true;
+    }
+    {
+      name = "DP-2";
+      width = 3840;
+      height = 2160;
+      workspace = "1";
+      primary = false;
+      position = "2160x0";
+      extraArgs = "transform,1";
+    }
+  ];
+
+  wayland.windowManager.hyprland.settings.device = [
+    {
+      name = "logitech-usb-receiver-mouse";
+      accel_profile = "adaptive";
+      sensitivity = 0.2;
+    }
+  ];
 }
