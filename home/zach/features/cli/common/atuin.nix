e85d89a6da885eacfa174f5e25480f0c823d2b98
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-07-17 11:04:32 +0200

    feat(hm): tweak atuin settings

--- home/zach/features/cli/common/atuin.nix
+++ home/zach/features/cli/common/atuin.nix
@@ -16,14 +16,19 @@ in {
       update_check = false;
       auto_sync = true;
       sync_address = syncAddress;
-      sync_frequency = "10m";
+      sync_frequency = "15m";
       db_path = "/persist${config.home.homeDirectory}/.atuin.db";
+      session_path = "/persist${config.home.homeDirectory}/.atuin-session";
 
       dialect = "uk";
-      style = "compact";
+      style = "auto";
       inline_height = 20;
+      prefers_reduced_motion = true;
+      search_mode = "skim";
       filter_mode = "host";
+      search_mode_shell_up_key_binding = "skim";
       filter_mode_shell_up_key_binding = "session";
+      workspaces = true;
       keymap_mode = "emacs";
       enter_accept = false;
       show_help = false;
