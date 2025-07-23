a7bf826d1ca10d17b185071e35fd6fd8d7e7c77b
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-07-23 12:25:27 +0200

    fix(hosts/jiji): adjust perms on mounted sb01

--- hosts/jiji/hardware.nix
+++ hosts/jiji/hardware.nix
@@ -49,7 +49,7 @@ in {
       fsType = "cifs";
       options =
         mountOptions
-        ++ ["uid=navidrome" "gid=users" "dir_mode=0775" "dir_mode=0664"];
+        ++ ["uid=navidrome" "gid=users" "dir_mode=0775" "file_mode=0664"];
     };
   };
 
