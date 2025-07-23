a12027eb91c94b7142a6d13e6eefc02437b4d391
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-07-23 12:16:38 +0200

    feat(hm/jiji): add beets

--- home/zach/jiji.nix
+++ home/zach/jiji.nix
@@ -2,4 +2,24 @@
   imports = [
     ./global
   ];
+
+  programs.beets = {
+    enable = true;
+
+    settings = {
+      library = "/storage/data01/beets/library.db";
+      directory = "/storage/sb01/music";
+      plugins = builtins.concatStringsSep " " [
+        "edit"
+        "fetchart"
+        "inline"
+        "thumbnails"
+      ];
+      paths = {
+        default = "$albumartist/$album%aunique{}/$track $title";
+        singleton = "Non-Album/$artist/$title";
+        comp = "Compilations/$album%aunique{}/$track $title";
+      };
+    };
+  };
 }
