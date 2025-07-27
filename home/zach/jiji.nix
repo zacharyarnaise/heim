f59625146d7cd6675e6a78101e10e6d37593bcb3
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-07-27 20:30:43 +0200

    feat(hm/jiji): disable fetchart for beets

--- home/zach/jiji.nix
+++ home/zach/jiji.nix
@@ -10,7 +10,6 @@
       library = "/storage/data01/beets/library.db";
       directory = "/storage/sb01/music";
       plugins = builtins.concatStringsSep " " [
-        "fetchart"
         "thumbnails"
         "edit"
       ];
@@ -23,16 +22,6 @@
         copy = false;
         move = false;
         write = true;
-      };
-      fetchart = {
-        cautious = true;
-        sources = "coverart itunes amazon albumart filesystem";
-        minwidth = 1200;
-        maxwidth = 1800;
-        enforce_ratio = true;
-        quality = 80;
-        max_filesize = 1048576;
-        cover_format = "JPEG";
       };
       match = {
         preferred = {
