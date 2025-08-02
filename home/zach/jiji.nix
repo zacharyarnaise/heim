4e0dfd24f8f7518e98e3e1a0fcb3773b7fe32ac0
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-08-02 13:55:30 +0200

    feat(hm/jiji): tweak beets settings

--- home/zach/jiji.nix
+++ home/zach/jiji.nix
@@ -9,6 +9,7 @@
     settings = {
       library = "/storage/data01/beets/library.db";
       directory = "/storage/sb01/music";
+      original_date = true;
       plugins = builtins.concatStringsSep " " [
         "thumbnails"
         "edit"
