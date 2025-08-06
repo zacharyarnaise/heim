159eec3b13384c724db5f62c8d1ec077859c5077
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-08-06 13:29:12 +0200

    feat(hm): beets config in its own file + persist state

---
+++ home/zach/features/cli/optional/beets.nix
@@ -0,0 +1,37 @@
+{config, ...}: {
+  programs.beets = {
+    enable = true;
+
+    settings = {
+      library = "/storage/data01/beets/library.db";
+      directory = "/storage/sb01/music";
+      original_date = true;
+      plugins = builtins.concatStringsSep " " [
+        "thumbnails"
+        "edit"
+      ];
+      paths = {
+        default = "$albumartist/$album%aunique{}/$track $title";
+        singleton = "Non-Album/$artist/$title";
+        comp = "Compilations/$album%aunique{}/$track $title";
+      };
+      import = {
+        copy = false;
+        move = false;
+        write = true;
+      };
+      match = {
+        preferred = {
+          media = ["CD" "Digital Media|File"];
+          original_year = true;
+        };
+      };
+    };
+  };
+
+  home.persistence."/persist${config.home.homeDirectory}" = {
+    files = [
+      ".config/beets/state.pickle"
+    ];
+  };
+}
--- home/zach/jiji.nix
+++ home/zach/jiji.nix
@@ -1,35 +1,7 @@
 {
   imports = [
     ./global
+
+    ./features/cli/optional/beets.nix
   ];
-
-  programs.beets = {
-    enable = true;
-
-    settings = {
-      library = "/storage/data01/beets/library.db";
-      directory = "/storage/sb01/music";
-      original_date = true;
-      plugins = builtins.concatStringsSep " " [
-        "thumbnails"
-        "edit"
-      ];
-      paths = {
-        default = "$albumartist/$album%aunique{}/$track $title";
-        singleton = "Non-Album/$artist/$title";
-        comp = "Compilations/$album%aunique{}/$track $title";
-      };
-      import = {
-        copy = false;
-        move = false;
-        write = true;
-      };
-      match = {
-        preferred = {
-          media = ["CD" "Digital Media|File"];
-          original_year = true;
-        };
-      };
-    };
-  };
 }
