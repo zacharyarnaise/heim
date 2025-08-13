f569d8d8c2f1f8caae460e9b5960c18ccf02af0e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-08-13 20:46:27 +0200

    fix(hosts/kamaji): fix wireguard conf

--- hosts/kamaji/wireguard.nix
+++ hosts/kamaji/wireguard.nix
@@ -1,7 +1,6 @@
 {
   config,
   inputs,
-  pkgs,
   ...
 }: let
   sopsSecrets = config.sops.secrets;
@@ -25,7 +24,6 @@ in {
     wg-quick.interfaces.wg0 = {
       type = "wireguard";
       address = ["10.2.0.2/32"];
-      dns = ["10.2.0.1"];
       listenPort = flakeSecrets.wg.port;
       mtu = 1380;
       privateKeyFile = sopsSecrets."wg/pk".path;
