61d07a66dfdceeb17889cc3db93ca7c3c54b0749
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-09-27 15:02:55 +0200

    feat: add direnv

--- home/zach/features/dev/direnv.nix
+++ home/zach/features/dev/direnv.nix
@@ -5,6 +5,7 @@
 }: {
   programs.direnv = {
     enable = true;
+    nix-direnv.enable = true;
     enableZshIntegration = config.programs.zsh.enable;
 
     config = {
@@ -12,9 +13,15 @@
         disable_stdin = true;
         strict_env = true;
       };
-      whitelist.prefix = lib.mkIf config.hostSpec.isWork [
-        "/persist${config.home.homeDirectory}/Code/Work/"
-      ];
+      whitelist.prefix =
+        [
+          "${config.home.homeDirectory}/Code/Nix/"
+          "/persist${config.home.homeDirectory}/Code/Nix/"
+        ]
+        ++ lib.optionals (config.hostSpec.isWork) [
+          "${config.home.homeDirectory}/Code/Work/"
+          "/persist${config.home.homeDirectory}/Code/Work/"
+        ];
     };
     silent = true;
   };
