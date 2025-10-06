3ab2fabee0c7c8b9b98409c69fc045feaa826cc9
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-10-06 16:17:01 +0200

    feat(hm/cli/dev): refactor git diff options

--- home/zach/features/dev/git.nix
+++ home/zach/features/dev/git.nix
@@ -1,46 +1,25 @@
 {
   config,
   inputs,
-  pkgs,
   lib,
   ...
 }: let
   inherit (inputs) secrets;
 in {
-  home.packages = builtins.attrValues {
-    inherit
-      (pkgs)
-      delta
-      difftastic
-      ;
-  };
-
   programs.git = {
     userName = "Zachary Arnaise";
     userEmail = "121795280+zacharyarnaise@users.noreply.github.com";
 
-    extraConfig = {
-      core.pager = "delta";
-      delta = {
-        features = [
-          "line-numbers"
-          "hyperlinks"
-          "side-by-side"
-          "commit-decoration"
-          "decorations"
-        ];
-        navigate = true;
-        light = false;
-      };
-
-      diff.tool = "difftastic";
-      difftool = {
-        prompt = "false";
-        difftastic.cmd = ''difft "$LOCAL" "$REMOTE"'';
-      };
-
-      format.signOff = "false";
+    difftastic = {
+      enable = true;
+      enableAsDifftool = true;
+      background = "dark";
+      color = "always";
+      display = "side-by-side";
+      context = 5;
     };
+
+    extraConfig.format.signOff = "false";
     signing.signByDefault = true;
 
     includes = lib.mkIf config.hostSpec.isWork [
