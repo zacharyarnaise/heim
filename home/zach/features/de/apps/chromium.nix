431ae26a517b0ca60a7afb66b821a4f1323d2cf0
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-10-09 19:09:05 +0200

    fix(hm/de/chromium): workaround for installing extensions

--- home/zach/features/de/apps/chromium.nix
+++ home/zach/features/de/apps/chromium.nix
@@ -1,57 +1,56 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  package = pkgs.ungoogled-chromium;
+in {
   programs.chromium = {
     enable = true;
+    inherit package;
 
-    package = pkgs.ungoogled-chromium;
+    commandLineArgs =
+      [
+        "--ozone-platform=wayland"
+        "--ozone-platform-hint=auto"
 
-    commandLineArgs = [
-      "--ozone-platform=wayland"
-      "--ozone-platform-hint=auto"
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+        "--enable-gpu-rasterization"
 
-      "--ignore-gpu-blocklist"
-      "--enable-zero-copy"
-      "--enable-gpu-rasterization"
+        "--disk-cache=$XDG_RUNTIME_DIR/chromium-cache" # cache in tmpfs
 
-      "--disk-cache=$XDG_RUNTIME_DIR/chromium-cache" # cache in tmpfs
+        "--disable-breakpad"
+        "--disable-speech-api"
+        "--disable-speech-synthesis-api"
+        "--disable-sync"
+        "--force-dark-mode"
+        "--no-crash-upload"
+        "--no-first-run"
+        "--no-service-autorun"
 
-      "--disable-breakpad"
-      "--disable-speech-api"
-      "--disable-speech-synthesis-api"
-      "--disable-sync"
-      "--force-dark-mode"
-      "--incognito"
-      "--no-crash-upload"
-      "--no-first-run"
-      "--no-service-autorun"
+        # ungoogled-chromium
+        # https://github.com/ungoogled-software/ungoogled-chromium/blob/master/docs/flags.md
+        "--disable-search-engine-collection"
+        "--extension-mime-request-handling=always-prompt-for-install"
+        "--fingerprinting-canvas-image-data-noise"
+        "--fingerprinting-canvas-measuretext-noise"
+        "--fingerprinting-client-rects-noise"
+        "--force-punycode-hostnames"
+        "--hide-crashed-bubble"
 
-      # ungoogled-chromium
-      # https://github.com/ungoogled-software/ungoogled-chromium/blob/master/docs/flags.md
-      "--disable-search-engine-collection"
-      "--extension-mime-request-handling=always-prompt-for-install"
-      "--fingerprinting-canvas-image-data-noise"
-      "--fingerprinting-canvas-measuretext-noise"
-      "--fingerprinting-client-rects-noise"
-      "--force-punycode-hostnames"
-      "--hide-crashed-bubble"
+        "--bookmark-bar-ntp=never"
+        "--close-confirmation=multiple"
+        "--close-window-with-last-tab=never"
+        "--disable-sharing-hub"
+        "--remove-grab-handle"
+        "--show-avatar-button=incognito-and-guest"
 
-      "--bookmark-bar-ntp=never"
-      "--close-confirmation=multiple"
-      "--close-window-with-last-tab=never"
-      "--custom-ntp=about:blank"
-      "--disable-sharing-hub"
-      "--remove-grab-handle"
-      "--show-avatar-button=incognito-and-guest"
+        "--disable-top-sites"
+        "--no-default-browser-check"
+        "--no-pings"
 
-      "--disable-top-sites"
-      "--no-default-browser-check"
-      "--no-pings"
-
-      "--enable-features=ClearDataOnExit,DisableQRGenerator,WebUIDarkMode"
-    ];
-
-    extensions = [
-      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # uBlock Origin
-      {id = "kkbncfpddhdmkfmalecgnphegacgejoa";} # Datadog test recorder
-    ];
+        "--enable-features=ClearDataOnExit,DisableQRGenerator,WebUIDarkMode"
+      ]
+      ++ map (extensionID: "https://clients2.google.com/service/update2/crx?response=redirect\\&acceptformat=crx2,crx3\\&prodversion=${package.version}\\&x=id%3D${extensionID}%26installsource%3Dondemand%26uc") [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+        "kkbncfpddhdmkfmalecgnphegacgejoa" # Datadog test recorder
+      ];
   };
 }
