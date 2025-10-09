32b9af90071804031d1e880655ebafa2ef03d6e2
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-10-09 14:00:36 +0200

    feat(hosts/calcifer): run audio DAC at higher sample rate

--- hosts/calcifer/hardware.nix
+++ hosts/calcifer/hardware.nix
@@ -28,6 +28,35 @@
     ];
   };
 
+  services.pipewire = {
+    extraConfig.pipewire = {
+      "10-clock-rate" = {
+        "context.properties" = {
+          "default.clock.allowed-rates" = [44100 48000 88200 96000];
+        };
+      };
+    };
+    wireplumber.extraConfig = {
+      "id24-sink-config" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {
+                "node.name" = "alsa_output.usb-Audient_Audient_iD24-00.multichannel-output";
+              }
+            ];
+            actions = {
+              update-props = {
+                "audio.format" = "S32LE";
+                "audio.rate" = 96000;
+              };
+            };
+          }
+        ];
+      };
+    };
+  };
+
   nix.settings.max-jobs = 28;
   hardware.cpu.intel.updateMicrocode = true;
   swapDevices = lib.mkForce [];
