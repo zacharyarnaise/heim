9f6ea9b1ca22dbbbb5009c6a30fafe220c36d372
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2025-12-26 11:27:06 +0100

    feat(hosts/jiji): tweak postgresql config

--- hosts/jiji/modules/postgresql.nix
+++ hosts/jiji/modules/postgresql.nix
@@ -1,12 +1,22 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_17;
 
-    dataDir = "/storage/data01/postgresql/17";
+    dataDir = "/storage/data01/postgresql/${config.services.postgresql.package.psqlSchema}";
     settings = {
+      effective_cache_size = "1GB";
+      effective_io_concurrency = 100;
+      huge_pages = "off";
       log_checkpoints = false;
-      shared_buffers = "128MB";
+      log_min_messages = "error";
+      maintenance_work_mem = "64MB";
+      shared_buffers = "256MB";
+      work_mem = "8MB";
     };
   };
 }
