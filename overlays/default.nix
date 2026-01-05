725b6a7cdc8cb95afc83f3b3a3d64b7b55b7db2e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-01-05 11:44:07 +0100

    fix(hm): vesktop build fail
    Until https://github.com/NixOS/nixpkgs/pull/476347 hits nixos-unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -45,6 +45,37 @@ in {
       databasePath = "/persist/etc/secureboot";
     };
 
+    # https://github.com/NixOS/nixpkgs/pull/476347
+    vesktop = prev.vesktop.overrideAttrs (_: {
+      # electron builds must be writable
+      preBuild =
+        prev.lib.optionalString prev.stdenv.hostPlatform.isDarwin ''
+          cp -r ${prev.electron.dist}/Electron.app .
+          chmod -R u+w Electron.app
+        ''
+        + prev.lib.optionalString prev.stdenv.hostPlatform.isLinux ''
+          cp -r ${prev.electron.dist} electron-dist
+          chmod -R u+w electron-dist
+        '';
+
+      buildPhase = ''
+        runHook preBuild
+
+        pnpm build
+        pnpm exec electron-builder \
+          --dir \
+          -c.asarUnpack="**/*.node" \
+          -c.electronDist=${
+          if prev.stdenv.hostPlatform.isDarwin
+          then "."
+          else "electron-dist"
+        } \
+          -c.electronVersion=${prev.electron.version}
+
+        runHook postBuild
+      '';
+    });
+
     waybar = addPatches prev.waybar [
       ./waybar_wireplumber_notfound.diff
     ];
