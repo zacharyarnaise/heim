995e5f7e27f0b5c304e48e3b7b2f5852d6ea5fb8
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-01-05 15:16:54 +0100

    fix(hm/hyprland): hyprpaper settings new syntax

--- home/zach/features/de/hyprland/hyprpaper.nix
+++ home/zach/features/de/hyprland/hyprpaper.nix
@@ -1,10 +1,19 @@
 {
+  lib,
+  config,
+  ...
+}: {
   services.hyprpaper = {
     enable = true;
 
-    settings = {
+    settings = lib.mkForce {
       ipc = false;
       splash = false;
+      # Workaround until https://github.com/nix-community/stylix/pull/2087 is merged
+      wallpaper = {
+        monitor = "";
+        path = config.stylix.image;
+      };
     };
   };
 }
