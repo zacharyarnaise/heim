2cab738f9eb7195189022c2a04aa1ff07d833b28
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-01-06 11:26:45 +0100

    fix(hosts): unlock gnome-keyring with ly

--- hosts/common/optional/keyring.nix
+++ hosts/common/optional/keyring.nix
@@ -4,7 +4,7 @@
     gcr-ssh-agent.enable = lib.mkForce false;
   };
   security.pam.services = {
-    greetd.enableGnomeKeyring = true;
     hyprlock.enableGnomeKeyring = true;
+    ly.enableGnomeKeyring = true;
   };
 }
