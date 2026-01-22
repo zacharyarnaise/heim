8f20892172b07faa4de7273d04bbd1fb67956667
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-01-22 22:23:55 +0100

    feat(hm/cli): tweak zsh settings

--- home/zach/features/cli/common/eza.nix
+++ home/zach/features/cli/common/eza.nix
@@ -5,8 +5,10 @@
 }: {
   programs.eza = {
     enable = true;
+    enableFishIntegration = lib.mkForce false; # Don't want default aliases
 
-    icons = "always";
+    extraOptions = ["--group-directories-first"];
+    icons = "auto";
     git = true;
   };
 
