16f3097342e65f644b802f5ac92064d2e0d5d99e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-04-14 22:24:28 +0200

    feat(hm/de): tweak hyprsunset profiles

--- home/zach/features/de/hyprland/hyprsunset.nix
+++ home/zach/features/de/hyprland/hyprsunset.nix
@@ -7,13 +7,16 @@
       {
         time = "08:00";
         identity = true;
+        gamma = 1.0;
       }
       {
         time = "20:00";
+        gamma = 0.8;
         temperature = 4000;
       }
       {
         time = "23:00";
+        gamma = 0.8;
         temperature = 3000;
       }
     ];
