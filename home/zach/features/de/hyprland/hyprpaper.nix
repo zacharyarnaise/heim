d80e8974952850d4b931f06a6ec4e96ca474e155
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-05-01 17:31:46 +0200

    fix(hm/de): clean hyprpaper config

--- home/zach/features/de/hyprland/hyprpaper.nix
+++ home/zach/features/de/hyprland/hyprpaper.nix
@@ -1,19 +1,10 @@
 {
-  lib,
-  config,
-  ...
-}: {
   services.hyprpaper = {
     enable = true;
 
-    settings = lib.mkForce {
+    settings = {
       ipc = false;
       splash = false;
-      # Workaround until https://github.com/nix-community/stylix/pull/2087 is merged
-      wallpaper = {
-        monitor = "";
-        path = config.stylix.image;
-      };
     };
   };
 }
