3178cc5f66e5d0858d526068444b90433755693e
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-05-09 19:07:38 +0200

    --wip--

--- home/zach/features/de/common/dms.nix
+++ home/zach/features/de/common/dms.nix
@@ -2,5 +2,8 @@
   imports = [inputs.dms.homeModules.dank-material-shell];
   programs.dank-material-shell = {
     enable = true;
+    systemd.enable = true;
   };
+
+  home.file.".config/DankMaterialShell/.firstlaunch".text = "";
 }
