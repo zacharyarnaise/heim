f808dc08a98ecee2a4269243d70fb2ce75b72312
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-05-26 10:57:02 +0200

    chore(lock): update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Inputs used by all configurations
