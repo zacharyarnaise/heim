a424e10225196ea2535d7d3c4f7b932899206209
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-06-10 18:47:41 +0200

    feat(hm/dev): add lazygit

--- home/zach/features/dev/git.nix
+++ home/zach/features/dev/git.nix
@@ -6,31 +6,54 @@
 }: let
   inherit (inputs) secrets;
 in {
-  programs.difftastic = {
-    enable = true;
-    git.enable = true;
-    git.diffToolMode = true;
-    options = {
-      background = "dark";
-      color = "always";
-      display = "side-by-side";
-      context = 5;
+  programs = {
+    difftastic = {
+      enable = true;
+      git.enable = true;
+      git.diffToolMode = true;
+      options = {
+        background = "dark";
+        color = "always";
+        display = "side-by-side";
+        context = 5;
+      };
     };
-  };
 
-  programs.git = {
-    settings = {
-      format.signOff = "false";
-      user.email = "121795280+zacharyarnaise@users.noreply.github.com";
-      user.name = "Zachary Arnaise";
+    git = {
+      settings = {
+        format.signOff = "false";
+        user.email = "121795280+zacharyarnaise@users.noreply.github.com";
+        user.name = "Zachary Arnaise";
+      };
+      signing.signByDefault = true;
+
+      includes = lib.mkIf config.hostSpec.isWork [
+        {
+          condition = "gitdir:${config.home.homeDirectory}/Code/Work/**";
+          contents = secrets.work.git;
+        }
+      ];
     };
-    signing.signByDefault = true;
 
-    includes = lib.mkIf config.hostSpec.isWork [
-      {
-        condition = "gitdir:${config.home.homeDirectory}/Code/Work/**";
-        contents = secrets.work.git;
-      }
-    ];
+    lazygit = {
+      enable = true;
+      settings = {
+        disableStartupPopups = true;
+        git = {
+          diffContextSize = 4;
+          mainBranches = ["main" "master" "develop"];
+        };
+        gui = {
+          commandLogSize = 5;
+          language = "en";
+          nerdFontsVersion = "3";
+          shortTimeFormat = "15:04:05 -0700";
+          showBranchCommitHash = true;
+          showNumstatInFilesView = true;
+          timeFormat = "Mon 02/01/2006 15:04:05 -0700";
+        };
+        update.method = "never";
+      };
+    };
   };
 }
