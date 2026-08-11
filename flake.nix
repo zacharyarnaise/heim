9b4d657bb3feced915778bb84381104d60be3c2d
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-08-11 09:23:51 +0200

    chore(lock): pin noctalia to latest beta tag

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia-shell/v5.0.0-beta.8";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
